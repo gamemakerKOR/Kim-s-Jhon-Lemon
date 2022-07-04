746fbffb21d3cbb4664e68d0a95340e0001c7ae7
gamemakerKOR <kyrwhdlwjqrl@naver.com>
2022-07-04 20:29:34 +0900

    Revert "음향효과 추가하기"
    This reverts commit 75dc77663146c64d60f42f7540ee4e38bc4c31c0.

--- 3D Beginner/Assets/Scripts/GameEnding.cs
+++ 3D Beginner/Assets/Scripts/GameEnding.cs
@@ -9,14 +9,11 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
-    public AudioSource exitAudio;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
-    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
-    bool m_HasAudioPlayed;
 
     void OnTriggerEnter(Collider other)
     {
@@ -35,22 +32,16 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+            EndLevel(exitBackgroundImageCanvasGroup, false);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
-        if (!m_HasAudioPlayed)
-        {
-            audioSource.Play();
-            m_HasAudioPlayed = true;
-        }
-
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
